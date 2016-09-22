da80d47a1a7365933f5cf8ebc1c60a6aa7a8ca20
Kevin Hurley <kphurley@gmail.com>
2016-09-22 13:14:07 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+var io = require('socket.io')(process.env.PORT || 3000);
+var shortid = require('shortid');
+
+console.log('server started');
+
+var players = [];
+
+io.on('connection', function(socket) {
+
+  var thisPlayerId = shortid.generate();
+  var thisPlayer = {id: thisPlayerId};
+
+  console.log('client id: ' + thisPlayerId + ' connected, broadcasting spawn');
+  socket.broadcast.emit('spawn');
+
+  players.push(thisPlayer);
+  console.log('Current connected players: ', players);
+
+  players.forEach(player => {
+    if(player.id == thisPlayerId){
+      return;
+    }
+    console.log('spawning player', player.id)
+    socket.emit('spawn', player);
+  })
+
+  socket.on('move', function(data) {
+    console.log('client id' + data.id + ' moved');
+  });
+
+  socket.on('disconnect', function() {
+    console.log('player with id '+ thisPlayerId +' disconnected');
+    players.splice(players.indexOf(thisPlayer), 1);
+  })
+
+})
